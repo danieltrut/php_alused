55a500fc2746cef3736a904cb22e19498b5e322b
danieltrut <Avdayg7410>
2020-11-24 10:25:25 +0200

    Ülesanne 1

---
+++ index.php
@@ -0,0 +1,17 @@
+
+
+<?php
+/*Ülesanne 1 Daniil Tr 24.11.2020*/
+//muutujate loomine
+    $nimi = 'Daniil Tr';
+    $synniaasta = "2020";
+    $kuu = "Jaanuar";
+
+    echo $nimi.'!<br>';
+    echo $synniaasta.'<br>';
+    echo $kuu.'<br>';
+
+    echo "(\(\<br>";
+    echo "( -.-)<br>";
+    echo '0_(")(")<br>';
+
