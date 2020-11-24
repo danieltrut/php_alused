888ed8b76448ad3147a98440bc37614e5163c351
danieltrut <Avdayg7410>
2020-11-24 11:33:06 +0200

    Ülesanne 3 trapetsi pindala

--- ülesanded/yl3.php
+++ ülesanded/yl3.php
@@ -1,37 +1,46 @@
-<body style="font-size: 22px;">
-<?php
-/*Ülesanne 3 Daniil Tr 24.11.2020*/
-//aritmeetilised operaatorid
-$x = 8;
-$y = 2;
-$liitm = $x + $y;
-$lahut = $x - $y;
-$korru = $x * $y;
-$jagam = $x / $y;
-$jaak = $x % $y;
-echo "Vastused: $liitm, $lahut, $korru, $jagam, $jaak!<br>";
-?>
-<div style="height: 50px;"></div>
-<?php
-//teisendamine
-$i = 726;
-$j = $i / 10;
-$k = $i / 1000;
-printf('Vastus meetrid:' .$k.'m' );
-echo "<br>";
-printf('Vastus sentimeetrit:' .$j.'sm');
-?>
-<div style="height: 50px;"></div>
-<?php
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
 
-//ümbermõõt ja pindala
-$uks = 8;
-$kaks = 2;
-$kolm = 6;
-$umbermuut = $uks + $kaks + $kolm;
-$pindala = ($kaks * $kolm)/2;
-echo "Ümbermõõt: $umbermuut!<br>";
-echo "Pindala: $pindala!<br>";
+<?php
+/*Ülesanne 3 Trapetsi pindala Daniil Tr 24.11.2020*/
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $korgus = test_input($_POST["korgus"]);
+    $pikkus1 = test_input($_POST["pikkus1"]);
+    $pikkus2 = test_input($_POST["pikkus2"]);
+
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
+<h2>Sisesta trapetsi kõrgus pikkus1 ja pikkus 2</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    Trapetsi Kõrgus: <input type="number" name="korgus">
+    <br><br>
+    Trapetsi pikkus 1: <input type="number" name="pikkus1">
+    <br><br>
+    Trapetsi pikkus 2: <input type="number" name="pikkus2">
+
+
+    <input type="submit" name="submit" value="Submit">
+</form>
+
+<?php
+$pindala = (($pikkus1+$pikkus2)/2)*$korgus;
+echo "<h2>Trapetsi pindala:</h2>";
+echo "<p style='color: red; font-size: 28px;'>$pindala</p>";
+
 
 ?>
+
 </body>
+</html>
